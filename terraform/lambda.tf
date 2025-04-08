36912e345455a04bf5f22137fba8a13f601f69a8
luke-h1 <luke.howsam@yahoo.com>
2025-04-08 14:46:48 +0100

    fix(lambda): change arch

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -20,6 +20,28 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+resource "aws_iam_policy" "xray_policy" {
+  name        = "${var.project_name}-${var.env}-xray-policy"
+  description = "Policy to allow X-Ray tracing for Lambda"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "xray_policy_attachment" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.xray_policy.arn
+}
 
 resource "aws_lambda_function" "lambda" {
   function_name    = "${var.project_name}-lambda-${var.env}"
@@ -32,6 +54,9 @@ resource "aws_lambda_function" "lambda" {
   description      = "blur hash Lambda ${var.env}"
   memory_size      = 128
   architectures    = ["x86_64"]
+  tracing_config {
+    mode = "active"
+  }
   environment {
     variables = {
       DEPLOYED_AT = timestamp()
