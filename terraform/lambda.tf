96fd361cdf3e1ed6710fbf0334f1a765dc3df093
luke-h1 <luke.howsam@yahoo.com>
2025-04-08 14:04:52 +0100

    fix(lambda): change arch

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,39 +1,20 @@
-data "archive_file" "lambda_archive" {
+# Create a Lambda Layer for sharp
+data "archive_file" "sharp_layer" {
   type        = "zip"
-  source_dir  = "${path.module}/../apps/lambda/dist"
-  output_path = "${path.module}/../lambda.zip"
+  source_dir  = "${path.module}/../sharp"
+  output_path = "${path.module}/../sharp.zip"
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "${var.project_name}-${var.env}-exec-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      }
-    ]
-  })
+resource "aws_lambda_layer_version" "sharp_layer" {
+  layer_name               = "${var.project_name}-sharp-layer-${var.env}"
+  compatible_runtimes      = ["nodejs22.x"]
+  compatible_architectures = ["x86_64"]
+  filename                 = data.archive_file.sharp_layer.output_path
+  source_code_hash         = data.archive_file.sharp_layer.output_base64sha256
+  description              = "Lambda Layer for sharp library"
 }
 
-
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# data "aws_iam_policy" "aws_xray_write_only_access" {
-#   arn = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
-# }
-# resource "aws_iam_role_policy_attachment" "aws_xray_write_only_access" {
-#   role       = aws_iam_role.lambda_exec.name
-#   policy_arn = data.aws_iam_policy.aws_xray_write_only_access.arn
-# }
-
+# Attach the sharp layer to the Lambda function
 resource "aws_lambda_function" "lambda" {
   function_name    = "${var.project_name}-lambda-${var.env}"
   runtime          = "nodejs22.x"
@@ -42,22 +23,23 @@ resource "aws_lambda_function" "lambda" {
   filename         = "${path.module}/../lambda.zip"
   source_code_hash = data.archive_file.lambda_archive.output_base64sha256
   timeout          = 10
-  # tracing_config {
-  #   mode = "Active"
-  # }
-  description   = "blur hash Lambda ${var.env}"
-  memory_size   = 128
-  architectures = ["x86_64"]
+  description      = "blur hash Lambda ${var.env}"
+  memory_size      = 128
+  architectures    = ["x86_64"]
   environment {
     variables = {
       DEPLOYED_AT = timestamp()
       DEPLOYED_BY = var.deployed_by
-      GIT_SHA : var.git_sha
+      GIT_SHA     = var.git_sha
     }
   }
   tags = merge(var.tags, {
     Environment = var.env,
   })
+
+  layers = [
+    aws_lambda_layer_version.sharp_layer.arn
+  ]
 }
 
 resource "aws_cloudwatch_log_group" "lambda_logs" {
